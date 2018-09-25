7c70ed7406489a1f2831d326640741eec542f7da
Aradhana <Varuna@Varuna-PC>
2018-09-25 19:58:32 +0530

    Changing method add

---
+++ Calculator/src/com/add/Calc.java
@@ -0,0 +1,19 @@
+package com.add;
+
+public class Calc {
+
+	public void add(){
+		
+		int a=0;
+		int b=8;
+		int c= a+b;
+		System.out.println("addition of a and b is "+c);
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Calc cal =new Calc();
+cal.add();
+	}
+
+}
